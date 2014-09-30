47acf2feb31396c182dd25ebc6f51d26726cfb46
GennaroC <catapano11@gmail.com>
2014-09-30 22:42:19 +0200

    One more exercise for Chapter 4

---
+++ Exercises/04 Particle Systems/0404.js
@@ -0,0 +1,534 @@
+//
+// The nature of code - Ch.3 Oscillation
+//
+// Exercise 4.4
+// Building off Chapter 3’s “Asteroids” example, 
+// use a particle system to emit particles from the ship’s “thrusters” whenever a thrust force is applied. 
+// The particles’ initial velocity should be related to the ship’s current direction.
+//
+// Written by: Gennaro Catapano
+//
+
+//
+// Exercise code
+//
+
+var mover,
+	mouseMoverDist,
+	gravity = new Vector2d(0,+0.01),
+	attritionCoeff = 0.1,
+	ps;
+
+function setup(canvas) {
+	mover = new Mover(canvas,30,canvas.width/2,canvas.height/2);
+	ps = new ParticleSystem(new Vector2d(canvas.width/2,canvas.height/2));
+	gravity.mult(mover.mass);
+};
+
+function update(canvas){
+	//mover.applyForce(gravity);
+	attrition = new Vector2d(mover.velocity.x,mover.velocity.y);
+	attrition.mult(-attritionCoeff);
+	mover.applyForce(attrition);
+	mover.checkEdges(canvas);
+	mover.update();
+
+	ps.update(mover.location,mover.velocity,mover.lastAcceleration,mover.angle);
+};
+
+function draw(context,canvas) {
+	background(context,canvas,"black");
+	//context.save();
+	
+	mover.display(context);
+	ps.display(context);
+
+	UI.display(context,canvas);
+};
+
+// Particle system object
+
+	function ParticleSystem(v2Location){
+		this.aParticles = [];
+		this.v2Origin = new Vector2d(v2Location.x,v2Location.y);
+		this.v2Acceleration = new Vector2d(0,0);
+		this.v2Velocity = new Vector2d(0,0);
+
+		this.iTopSpeed = 10;
+
+		this.iParticlesPerCycle = 8;
+		this.v2NewParticlesAcc;
+	};
+
+	ParticleSystem.prototype = {
+		addParticle : function(v2Location){
+			this.aParticles.push(new Particle(new Vector2d(v2Location.x,v2Location.y),
+												Math.random()*5,
+												new Vector2d(this.v2NewParticlesAcc.x + (Math.random()-0.5)*0.01,
+													this.v2NewParticlesAcc.y + (Math.random()-0.5)*0.01)));
+		},
+		update: function(v2MoverLocation,v2MoverVelocity,v2MoverAcceleration,iAngle){
+
+			//this.v2Velocity.add(this.v2Acceleration);
+			//this.v2Velocity.limit(this.iTopSpeed);
+			//this.v2Origin.add(this.v2Velocity);
+			this.v2Origin = new Vector2d(v2MoverLocation.x + 35*Math.cos(iAngle -Math.PI/2),v2MoverLocation.y + 35*Math.sin(iAngle -Math.PI/2));
+			this.v2NewParticlesAcc = new Vector2d(v2MoverAcceleration.x,v2MoverAcceleration.y);
+			this.v2NewParticlesAcc.mult(-1*0.1);
+
+			if (this.v2NewParticlesAcc.mag() > 0.01) {
+				for (var i = 0; i < this.iParticlesPerCycle; i++){
+					var v2OrtVector = new Vector2d(this.v2Origin.y,this.v2Origin.x*-1);
+					v2OrtVector.mult(0.1);
+					var v2ParticleLocation = Vector2d.prototype.add(this.v2Origin,v2OrtVector);
+
+					this.addParticle(v2ParticleLocation);
+				};
+			};
+
+			for (var i = 0; i < this.aParticles.length; i++) {
+				if (this.aParticles[i].isDead()) {
+					this.aParticles.splice(i,1);
+					i--;
+				} else {
+					this.aParticles[i].update();
+				};
+			};
+
+			this.v2Acceleration.mult(0);
+		},
+		display: function(context){
+			for (var i = 0; i < this.aParticles.length; i++) {
+				this.aParticles[i].display(context);
+			};
+		},
+		applyForce: function(v2Force){
+			this.v2Acceleration.add(v2Force);
+		}
+	};
+//
+
+// Particle object
+
+	function Particle(v2Location,iMass,v2Acceleration) {
+		this.location = v2Location;
+		this.velocity = new Vector2d(0,0);
+		this.acceleration = v2Acceleration;
+		this.lifespan = 1.5;
+		this.mass = iMass;
+
+		this.angle = 0;
+		this.aAcceleration = 0;
+		this.aVelocity = 0;
+
+		this.topSpeed = 5;
+
+		this.colorRed = 256;
+		this.colorBlue = 0;
+	};
+
+	Particle.prototype = {
+		update: function(){
+			this.velocity.add(this.acceleration);
+			this.velocity.limit(this.topSpeed);
+			this.location.add(this.velocity);
+			this.lifespan -= 0.01;
+
+			this.aAcceleration = this.velocity.x;
+			this.aVelocity += this.aAcceleration;
+			this.angle += this.aVelocity;
+
+			// color change
+			if (this.mass > 1 && Math.random() > 0.85) {this.mass += 0.25;};
+			if (Math.random() >= 0.5) {
+				if (this.colorRed > 0 && this.colorBlue <= 256) {
+					this.colorBlue += 3;
+					this.colorRed  -= 3;
+				};
+				if (this.colorRed <= 0 && this.colorBlue >= 0) {
+					this.colorBlue -= 3;
+					//this.mass += 1;
+				};
+			};
+		},
+		display: function(context){
+			context.save();
+			context.translate(this.location.x,this.location.y);
+			context.rotate(this.angle*Math.PI/180);
+			if (this.lifespan < 0) {
+				context.globalAlpha = 0;
+			} else {
+				context.globalAlpha = this.lifespan;
+			};
+			context.fillStyle = "rgb(" + this.colorRed + " , 0, " + this.colorBlue + ")";
+			context.fillRect(-this.mass/2, -this.mass/2, this.mass, this.mass);
+			//rectangle(context,-10,-10,20,20);
+			context.restore();
+		},
+		isDead: function(){
+			if (this.lifespan < 0.0){
+				return true;
+			} else {
+				return false;
+			};
+		},
+		applyForce: function(v2Force){
+			var _v2Force = Vector2d.prototype.div(this.mass,v2Force);
+			this.acceleration.add(_v2Force);
+		}
+	};
+//
+
+// Mover object (Js has no classes)
+	function Mover (canvas,m,x,y){
+		this.location = new Vector2d(
+			x,
+			y
+			);
+		this.velocity = new Vector2d(
+			0,
+			-0.001
+			);
+		this.acceleration = new Vector2d(
+			0,
+			0
+			);
+		this.lastAcceleration = new Vector2d(
+			0,
+			0
+			);
+		this.topSpeed = 5;
+		this.mass = m;
+		this.G = 1.5;
+
+		this.angle = 0;
+		this.aVelocity = 0;
+		this.aAcceleration = 0;
+	}
+
+	Mover.prototype = {
+		update: function(){
+			this.velocity.add(this.acceleration);
+			this.velocity.limit(this.topSpeed);
+			this.location.add(this.velocity);
+
+			//this.aAcceleration = this.acceleration.x * 10;
+			//this.aVelocity += this.aAcceleration;
+			//this.angle += this.aVelocity;
+
+			//this.angle = Math.atan2(this.velocity.y, this.velocity.x) - Math.PI/2;
+
+			this.lastAcceleration = new Vector2d(this.acceleration.x,this.acceleration.y);
+			this.acceleration.mult(0);
+		},
+		display: function(context){
+			context.save();
+
+			context.translate(this.location.x,this.location.y);
+			//context.rotate(this.angle*Math.PI/180);
+			context.rotate(this.angle);
+
+			//rectangle(context, -this.mass, -this.mass/2, this.mass*2, this.mass);
+			//ellipse(context,this.location.x,this.location.y,this.mass*2);		
+
+			var a = new Vector2d(-20,-10);
+				b = new Vector2d( 20,-10);
+				c = new Vector2d(  0, 50);
+
+			triangle(context,a,b,c);
+			rectangle(context, -15, -20, 8, 10);
+			rectangle(context,   8, -20, 8, 10);
+
+			context.restore();
+		},
+		checkEdges: function(canvas) {
+			if (this.location.x > canvas.width) {
+				//this.location.x = 0;
+				this.location.x = canvas.width;
+				this.velocity.x *= -1;
+			} else if (this.location.x < 0) {
+				//this.location.x = canvas.width;
+				this.velocity.x *= -1;
+				this.location.x = 0;
+			}
+
+			if (this.location.y > canvas.height) {
+				//his.location.y = 0;
+				this.velocity.y *= -1;
+				this.location.y = canvas.height;
+			} else if (this.location.y < 0) {
+				//this.location.y = canvas.height;
+				this.velocity.y *= -1;
+				this.location.y = 0;
+			}
+		},
+		applyForce: function(vector2d){
+			var f = Vector2d.prototype.div(this.mass,vector2d);
+			this.acceleration.add(f);
+		},
+		isInside: function(obj){
+			if (this.location.x > obj.x && this.location.x < (obj.x+obj.w) &&
+				this.location.y > obj.y && this.location.y < (obj.y+obj.h)) {
+					return true;
+				} else {
+					return false;
+				};				
+		},
+		drag: function(liquid){
+			var speed = this.velocity.mag();
+			var dragMagnitude = liquid.c * speed * speed;
+
+			var drag = Vector2d.prototype.mult(1,this.velocity);
+			drag.mult(-1);
+			drag.normalize();
+
+			drag.mult(dragMagnitude);
+
+			this.applyForce(drag);
+		},
+		attract: function(mover){
+			var force = Vector2d.prototype.sub(this.location,mover.location);
+			var distance = force.mag();
+
+			if (distance < 15 ) { distance = 15;};
+			if (distance > 400) {return new Vector2d(0,0);
+			} else { 
+				force.normalize();
+				var strength = (this.G * this.mass * mover.mass) / (distance * distance);
+				force.mult(strength);
+
+				return force;
+			};
+		}
+	};
+//
+
+// Attractor object
+
+	function Attractor(canvas) {
+		this.mass = 20;
+		this.location = new Vector2d(canvas.width/2,canvas.height/2);
+		this.G = 3;
+	};
+
+	Attractor.prototype = {
+		display: function(context){
+			ellipse(context,this.location.x,this.location.y,this.mass*2);
+		},
+		attract: function(mover){
+			var force = Vector2d.prototype.sub(this.location,mover.location);
+			var distance = force.mag();
+
+			if (distance < 5) {distance=5;};
+			if (distance > 400) {return new Vector2d(0,0);
+			} else { 
+				force.normalize();
+				var strength = (this.G * this.mass * mover.mass) / (distance * distance);
+				force.mult(strength);
+
+				return force;
+			};
+		}
+	};
+//
+
+// mouseHandler
+	function mouseHandler(canvas){
+		canvas.onmousemove = function(e){
+			mouse.x = e.clientX;
+			mouse.y = e.clientY;
+		};
+	};
+// 
+
+// keyboardHandler
+	function keyHandler(canvas){
+		document.addEventListener('keydown', function(event) {
+		    if (event.keyCode == 33) {
+		        //alert('Page up');
+		    }
+		    else if (event.keyCode == 34) {
+		        //alert('Page down');
+		    }
+		    else if (event.keyCode == 38) {
+		        //alert('up');
+
+		        // this next commented line is wrong and just references to the velocity vector 
+		        // without actually creating a copy of it
+		        //var acc = mover.velocity;
+
+		        //var acc = Vector2d.prototype.mult(1,mover.velocity);
+		        //acc.normalize();
+		        //acc.mult(mover.mass/10);
+		        //mover.applyForce(acc);
+
+		        var acc = new Vector2d(Math.cos(mover.angle + Math.PI/2),Math.sin(mover.angle + Math.PI/2));
+		        acc.mult(5);
+		        mover.applyForce(acc);
+		    }
+		    else if (event.keyCode == 40) {
+		        //alert('down');
+		        var acc = Vector2d.prototype.mult(1,mover.velocity);
+		        acc.mult(-mover.mass/10);
+		        mover.applyForce(acc);
+		    }
+		    else if (event.keyCode == 37) {
+		        //alert('left');
+		        //var acc = new Vector2d(mover.velocity.y,-mover.velocity.x);
+		        //acc.mult(mover.mass/10);
+		        //mover.applyForce(acc);
+
+		        mover.angle -= 0.1;
+		    }
+		    else if (event.keyCode == 39) {
+		        //alert('right');
+		        //var acc = new Vector2d(-mover.velocity.y,mover.velocity.x);
+		        //acc.mult(mover.mass/10);
+		        //mover.applyForce(acc);
+
+		        mover.angle += 0.1;
+		    }
+		    else if (event.keyCode == 32) {
+		        //alert('space');
+		    }
+		}, true);
+	};
+//
+
+// UI
+
+	var UI = {
+
+		barLength : 150,
+		barHeight : 50,
+		barPadding : 2,
+		barMargin : 10,
+		xOrigin : 20,
+		yOrigin: 20,
+
+		display: function(context,canvas){
+			context.font="20px Georgia";
+			context.fillStyle = "White";
+			var speed = Math.round(mover.velocity.mag()*100)/100;
+			context.fillText("Speed: " + speed,10,50);
+			var acceleration = Math.round(mover.lastAcceleration.mag()*100)/100;
+			context.fillText("Acceleration: " + acceleration,10,80);
+		}
+
+	};
+
+//
+
+//
+// main()
+// Canvas and Loop init
+//
+
+(function main(){
+	// std variables
+	var backgroundColor = "Black",
+		viewportHeight = 800,
+		viewportWidth = 800,
+		viewportId = "viewport",
+		timeStep = 1000 / 30,
+		canvas,
+		context;
+
+	// canvas & context creation
+	window.onload = function() {
+		canvas = document.createElement("canvas");
+		canvas.setAttribute("width", viewportWidth);
+		canvas.setAttribute("height", viewportHeight);
+		canvas.setAttribute("id", viewportId);
+		document.getElementById("canvasPlace").appendChild(canvas);
+		context = canvas.getContext("2d");
+
+		context.fillStyle = backgroundColor;
+		context.fillRect(0,0,canvas.width,canvas.height);
+
+		Loop();
+	};
+
+	// main loop
+	function Loop(){
+		requestAnimationFrame(updateWorld);
+
+		(function init(){
+			// initialize stuff here
+			 //context.globalAlpha = 0.25;
+			 //mouseHandler(canvas);
+			 keyHandler(canvas);
+			 setup(canvas);
+			 //
+
+
+		}());
+		function updateWorld(){
+			// update logic here
+			 //
+			 update(canvas);
+			 render();
+			 //
+		}
+		function render(){
+			// rendering logic here
+			 //
+			 draw(context,canvas);
+			 //
+			requestAnimationFrame(updateWorld);
+		}
+	};
+}());
+
+//
+// helper functions ellipse(), background() etc
+//
+
+var helpersStrokeStyle = "white";
+var helpersFillStyle = "grey";
+var lineWidth = 4;
+
+function ellipse(context,x,y,radius){
+	context.fillStyle = helpersFillStyle;
+	context.strokeStyle = helpersStrokeStyle;
+	context.lineWidth = lineWidth;
+	context.beginPath();
+	context.arc(x,y,radius,0,2*Math.PI);
+	context.fill();
+	context.stroke();
+}
+
+function background(context,canvas,color){
+	context.fillStyle = color;
+	context.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function line(context,x0,y0,x1,y1){
+	context.strokeStyle = helpersStrokeStyle;
+	context.lineWidth = lineWidth;
+	context.beginPath(); // resets the previous path
+	context.moveTo(x0,y0); // no translation f() so had to be done manually
+	context.lineTo(x1,y1); // no translation f() so had to be done manually
+	context.stroke();
+}
+
+function rectangle(context, x, y, width, height){
+	context.fillStyle = helpersStrokeStyle;
+	context.fillRect(x, y, width, height);
+
+	context.fillStyle = helpersFillStyle;
+	context.fillRect(x+lineWidth/2,y+lineWidth/2,width-lineWidth,height-lineWidth);
+}
+
+function triangle(context, A, B, C){
+	context.fillStyle = helpersFillStyle;
+	context.strokeStyle = helpersStrokeStyle;
+	context.lineWidth = lineWidth;
+	context.beginPath();
+    context.moveTo(A.x,A.y);
+    context.lineTo(B.x,B.y);
+    context.lineTo(C.x,C.y);
+    context.lineTo(A.x,A.y);
+    context.fill();
+    context.stroke();
+}
